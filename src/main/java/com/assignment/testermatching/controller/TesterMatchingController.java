d473e4b481f75f5ac1316a8ef320d6185e5bced2
rissekwow <w.tomaszewski92@gmail.com>
2019-10-27 23:20:08 +0100

    bugfix/wrong-request-mapping

--- src/main/java/com/assignment/testermatching/controller/TesterMatchingController.java
+++ src/main/java/com/assignment/testermatching/controller/TesterMatchingController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value = "/testermatching")
+@RequestMapping(value = "/tester")
 @CrossOrigin(origins = "http://localhost:4200")
 public class TesterMatchingController {
 
